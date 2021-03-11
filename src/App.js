2f5fcad99f0face43b657bacc36785b4b591cdce
MuhammadSalman009-tech <salmansumra009@gmail.com>
2021-03-12 01:07:17 +0500

    adding MenuComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import {Navbar, NavbarBrand} from "reactstrap";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">
@@ -11,6 +12,8 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+
+      <Menu />
     </div>
   );
 }
