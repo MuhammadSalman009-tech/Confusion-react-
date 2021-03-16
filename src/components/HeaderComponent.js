78b4afa2a2f1281cc294eefd6b73a57f406ac4d9
MuhammadSalman009-tech <salmansumra009@gmail.com>
2021-03-16 16:02:59 +0500

    Uncontrolled forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import {Navbar, NavbarBrand, Jumbotron, Nav, Collapse, NavItem, NavbarToggler} from "reactstrap";
+import {Navbar, NavbarBrand, Jumbotron, Nav, Collapse, NavItem, NavbarToggler,
+Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input} from "reactstrap";
 class Header extends Component{
     constructor(props){
         super(props);
         this.state={
-            isNavOpen:false
+            isNavOpen:false,
+            isModalOpen: false
         }
         this.toggleNav=this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
     toggleNav(){
         this.setState({
             isNavOpen:!this.state.isNavOpen
         })
     }
+    toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+      }
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+    }  
     render(){
         return(
         <React.Fragment>
@@ -46,6 +62,11 @@ class Header extends Component{
                             </NavLink>
                         </NavItem>
                     </Nav>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <Button color="primary" onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                        </NavItem>
+                    </Nav>
                 </Collapse>
             </div>
             </Navbar>
@@ -59,6 +80,31 @@ class Header extends Component{
                     </div>
                 </div>
             </Jumbotron>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                        <FormGroup>
+                            <Label htmlFor="username">Username</Label>
+                            <Input type="text" id="username" name="username"
+                                innerRef={(input) => this.username = input} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="password">Password</Label>
+                            <Input type="password" id="password" name="password"
+                                innerRef={(input) => this.password = input}  />
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" name="remember"
+                                innerRef={(input) => this.remember = input}  />
+                                Remember me
+                            </Label>
+                        </FormGroup>
+                        <Button type="submit" value="submit" color="primary">Login</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
         </React.Fragment>
         );
     }
